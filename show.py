1e103666d80ea4f7b68526ea53dd2882575f26d0
Sanjeev <sanjeev-ku@moving-picture.com>
2013-03-08 11:27:06 +0530

    accepts more than one environment variables.

--- show.py
+++ show.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env mpcpython
 ################################
 ## Sanjeev
-## Version: 1.0
+## Version: 1.1
 ################################
 import os, sys
-variable  = ""
-try:
-        nVar = len(sys.argv)-1
-	variable =  sys.argv[1]
-except:
-	variable = None
 
-def enVar(variable=None):
+variable =  sys.argv
+
+def enVar(variable):
         """
         This function returns all the environment variable set on the machine or in active project.
         if environment variable name is passed to the enVar function it returns its values.
         """
-        if variable==None:
+        nVar = len(sys.argv)-1
+        if len(variable)== 1: # if user entered no environment variable name
                 for index, each in enumerate(os.environ.iteritems()):
                         print index, each
-        else:
-
-        	if os.environ.get(variable):
-        		print "%s : %s" %  (variable, os.environ.get(variable))
-        	else: print 'Make sure the Environment variable "%s" exists or spelled correctly.' % variable
+        else: # if user entered one or more than one environment variable name
+                for x in range(nVar):
+                        x+=1
+                        if os.environ.get(variable[x]):
+                                print "%s : %s" %  (variable[x], os.environ.get(variable[x]))
+        	        else: print 'Make sure the Environment variable "%s" exists or spelled correctly.' % variable[x]
 
 enVar(variable)
