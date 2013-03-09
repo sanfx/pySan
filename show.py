194c84060304189d42f4a00ca241d2a87ac97240
Sanjeev <niceguysan@gmail.com>
2013-03-09 21:42:51 +0530

    code updated

--- show.py
+++ show.py
@@ -1,26 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python26
 ################################
 __author__  = "Sanjeev"
 __version__ = 1.1
 ################################
-import os, sys
+import os
+import sys
 
-variable =  sys.argv
+print sys.argv[1:]
 
-def enVar(variable):
+def environment_variables(variable):
         """
         This function returns all the environment variable set on the machine or in active project.
         if environment variable name is passed to the enVar function it returns its values.
         """
         nVar = len(sys.argv)-1
-        if len(variable) == 1: # if user entered no environment variable name
+        print any(variable)
+        if not any(variable): # if user entered no environment variable name
                 for index, each in enumerate(sorted(os.environ.iteritems())):
-                        print index, each
+                        print "%s %s: %s" %(index, each[0], each[1])
         else: # if user entered one or more than one environment variable name
                 for x in range(nVar):
-                        x+=1
                         if os.environ.get(variable[x].upper()): # convertes to upper if user mistakenly enters lowecase
                                 print "%s : %s" %  (variable[x].upper(), os.environ.get(variable[x].upper()))
         	        else: print 'Make sure the Environment variable "%s" exists or spelled correctly.' % variable[x]
 
-enVar(variable)
+environment_variables(sys.argv[1:])
