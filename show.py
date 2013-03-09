2ea87f2a4a99abf4cc12badb9fa9ee47ae7a8996
Sanjeev <niceguysan@gmail.com>
2013-03-09 22:53:19 +0530

    python version 3 code optimized

--- show.py
+++ show.py
@@ -2,21 +2,16 @@
 ################################
 __author__  = "Sanjeev"
 __version__ = 1.1
+## Branch: python3_ver2
 ################################
 import sys
 import os
 
 def print_environment_variables(variables):
     """Print environment variables (all if variables list is empty)."""
-    if not variables:
-        for index, (variable, value) in enumerate(sorted(os.environ.items())):
-            print("{0}: {1}".format(variable, value))
-    else: 
-        for variable in variables:
-          value = os.environ.get(variable)
-          if value:
-              print("{0}: {1}".format(variable, value))
-          else:
-            print("Variable does not exist: {0}".format(variable))
+    variables_to_show = variables or sorted(os.environ.keys())
+    for index, variable in enumerate(variables_to_show):
+        value = os.environ.get(variable) or "[variable does not exist]"
+        print("{0}: {1}={2}".format(index, variable, value))
 
 print_environment_variables(sys.argv[1:])
