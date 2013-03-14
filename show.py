a5fc1195145e6e293d78bd8b211890979abfdf99
Sanjeev <sanjeev-ku@moving-picture.com>
2013-03-14 12:49:01 +0530

    changed from functional approach to OOP based

--- show.py
+++ show.py
@@ -3,24 +3,28 @@
 __author__  = "Sanjeev"
 __version__ = 1.1
 ################################
-import os, sys
+import os
+import sys
 
-variable =  sys.argv
 
-def enVar(variable):
-        """
-        This function returns all the environment variable set on the machine or in active project.
-        if environment variable name is passed to the enVar function it returns its values.
-        """
-        nVar = len(sys.argv)-1
-        if len(variable) == 1: # if user entered no environment variable name
-                for index, each in enumerate(sorted(os.environ.iteritems())):
-                        print index, each
-        else: # if user entered one or more than one environment variable name
-                for x in range(nVar):
-                        x+=1
-                        if os.environ.get(variable[x].upper()): # convertes to upper if user mistakenly enters lowecase
-                                print  "%s : %s" %  (variable[x].upper(), os.environ.get(variable[x].upper()))
-        	        else: print 'Make sure the Environment variable "%s" exists or spelled correctly.' % variable[x]
+class Show(object):
+        def __init__(self):
+                #self.environment_variables(sys.argv[1:])
+                pass
 
-enVar(variable)
+
+        def environment_variables(self,variable):
+                """
+                This function returns all the environment variable set on the machine or in active project.
+                if environment variable name is passed to the enVar function it returns its values.
+                """
+                nVar = len(sys.argv)-1
+
+                if not any(variable): # if user entered no environment variable name
+                        for index, each in enumerate(sorted(os.environ.iteritems())):
+                                print "%s %s: %s" %(index, each[0], each[1])
+                else: # if user entered one or more than one environment variable name
+                        for x in range(nVar):
+                                if os.environ.get(variable[x].upper()): # convertes to upper if user mistakenly enters lowecase
+                                        print "%s : %s" % (variable[x].upper(), os.environ.get(variable[x].upper()))
+                                else: print 'Make sure the Environment variable "%s" exists or spelled correctly.' % variable[x]
